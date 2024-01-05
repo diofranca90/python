e093c5c4c3c0a987ca3f31760bb8962df4c62d98
Diorgenes França <74383792+diofranca90@users.noreply.github.com>
2024-01-05 15:05:40 -0300

    Add files via upload

---
+++ 5 - for.py
@@ -0,0 +1,21 @@
+# for sistema de repetição
+# x é o nome da variável que vai receber os elemento da repetição
+
+# exemplo sem condição
+lista = [1, 2, 3, 4, 5]
+
+for x in lista:
+    print(x)
+
+# exemplo com condição
+sequencia = [0, 1, 2, 3, 4, 5]
+
+for x in sequencia:
+    print(x)
+else:
+    print('Todos os items foram exibidos com sucesso')    
+
+# exmplo de repetição de cada caracter da palavra atribuida ex: Pytohn
+for x in 'Python':
+    print(x)
+   
---
+++ 6 - for com dicionario.py
@@ -0,0 +1,12 @@
+# for sistema de repetição
+# exemplo com dicionário chave-valor
+
+notas = {
+    'Potuguês': 7, 
+    'Matemática': 9, 
+    'Lógica': 7, 
+    'Algoritmo': 7
+}
+
+for chave, valor in notas.items():
+    print(f"{chave}: {valor}") 
